fda275c91da56f04862cd60c1db02ec9c3dee998
karim <kmkg2001@gmail.com>
2024-09-08 04:58:33 +0300

    k

--- frontend/src/hooks/useInitData.ts
+++ frontend/src/hooks/useInitData.ts
@@ -1,12 +1,10 @@
 import { useContext } from "react";
 import { AppContext } from "../contexts/AppContext";
-import { useNavigate } from "react-router-dom";
 
 export default function useInitData() {
   const { setData, setLoading } = useContext(AppContext);
 
   const initData = async () => {
-    //const navigate = useNavigate();
     setLoading(true);
     try {
       const response = await fetch("https://demo-backend-vercel.fly.dev/api/", {
